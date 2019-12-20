417612636e8e724ae4a5553edd3dc6fd0c71acd3
zainshabir <zainshabir00@gmail.com>
2019-12-20 12:58:51 +0500

    updated tablet screen, form and phone number format

--- build/developing/js/scripts/ajaxify_form.js
+++ build/developing/js/scripts/ajaxify_form.js
@@ -4,8 +4,10 @@ $("#quoteForm").submit(function(event) {
 });
 
 function sendFormMessage() {
-    var valid;
-    valid = validateQuoteForm();
+    var valid = true;
+    $("select,.form-item--input").each(function(){ 
+        valid = validateInput(this);
+    });
     $('.form-errors').empty();
     $('.form-errors').hide();
     if (valid === true) {
@@ -45,90 +47,113 @@ function sendFormMessage() {
 }
 
 
-function validateQuoteForm() {
+// function validateQuoteForm() {
     
-    var yearValue = $(".quote-form--year").val();
-    var makeValue = $(".quote-form--make").val();
-    var modelValue = $(".quote-form--model").val();
-    var milageValue = $(".quote-form--milage").val();
-    var firstNameValue = $(".quote-form--firstName").val();
-    var lastNameValue = $(".quote-form--lastName").val();
-    var emailValue = $(".quote-form--email").val();
-    var phoneValue = $(".quote-form--phone").val();
-    var zipValue = $(".quote-form--zip").val();
-    var valid = "";
+//     var yearValue = $(".quote-form--year").val();
+//     var makeValue = $(".quote-form--make").val();
+//     var modelValue = $(".quote-form--model").val();
+//     var mileageValue = $(".quote-form--mileage").val();
+//     var firstNameValue = $(".quote-form--firstName").val();
+//     var lastNameValue = $(".quote-form--lastName").val();
+//     var emailValue = $(".quote-form--email").val();
+//     var phoneValue = $(".quote-form--phone").val();
+//     var zipValue = $(".quote-form--zip").val();
+//     var valid = "";
 
-    $('input,textarea,.form-item').removeClass('error');
-    $('.form-item').removeClass('input-error');
-    $('.form-item .desc').hide();
+//     $('input,textarea,.form-item').removeClass('error');
+//     $('.form-item').removeClass('input-error');
+//     $('.form-item .desc').hide();
 
-    if (!yearValue.trim()) {
-        $('.form-item-year .desc').show();
-        $('.form-item-year').addClass('error');
-        $('.form-item-year').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!makeValue.trim()) {
-        $('.form-item-make .desc').show();
-        $('.form-item-make').addClass('error');
-        $('.form-item-make').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!modelValue.trim()) {
-        $('.form-item-model .desc').show();
-        $('.form-item-model').addClass('error');
-        $('.form-item-model').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!milageValue.trim()) {
-        $('.form-item-milage .desc').show();
-        $('.quote-form--milage').addClass('error');
-        $('.quote-form--milage').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!firstNameValue.trim()) {
-        $('.form-item-firstName .desc').show();
-        $('.quote-form--firstName').addClass('error');
-        $('.quote-form--firstName').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!lastNameValue.trim()) {
-        $('.form-item-lastName .desc').show();
-        $('.quote-form--lastName').addClass('error');
-        $('.quote-form--lastName').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!emailValue.trim()) {
-        $('.form-item-email .desc').show();
-        $('.quote-form--email').addClass('error');
-        $('.quote-form--email').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!emailValue.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
-        $('.form-item-email .desc').text('Email is not valid');
-        $('.form-item-email .desc').show();
-        $('.quote-form--email').addClass('error');
-        $('.quote-form--email').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!phoneValue.trim()) {
-        $('.form-item-phone .desc').show();
-        $('.quote-form--phone').addClass('error');
-        $('.quote-form--phone').parent().addClass('input-error');
-        valid = false;
-    }
-    if (!zipValue.trim()) {
-        $('.form-item-zip .desc').show();
-        $('.quote-form--zip').addClass('error');
-        $('.quote-form--zip').parent().addClass('input-error');
-        valid = false;
-    }
+//     if (!yearValue.trim()) {
+//         $('.form-item-year .desc').show();
+//         $('.form-item-year').addClass('error');
+//         valid = false;
+//     }
+//     if (!makeValue.trim()) {
+//         $('.form-item-make .desc').show();
+//         $('.form-item-make').addClass('error');
+//         valid = false;
+//     }
+//     if (!modelValue.trim()) {
+//         $('.form-item-model .desc').show();
+//         $('.form-item-model').addClass('error');
+//         valid = false;
+//     }
+//     if (!mileageValue.trim()) {
+//         $('.form-item-mileage .desc').show();
+//         $('.quote-form--mileage').addClass('error');
+//         $('.quote-form--mileage').parent().addClass('input-error');
+//         valid = false;
+//     }
+//     if (!firstNameValue.trim()) {
+//         $('.form-item-firstName .desc').show();
+//         $('.quote-form--firstName').addClass('error');
+//         $('.quote-form--firstName').parent().addClass('input-error');
+//         valid = false;
+//     }
+//     if (!lastNameValue.trim()) {
+//         $('.form-item-lastName .desc').show();
+//         $('.quote-form--lastName').addClass('error');
+//         $('.quote-form--lastName').parent().addClass('input-error');
+//         valid = false;
+//     }
+//     if (!emailValue.trim()) {
+//         $('.form-item-email .desc').show();
+//         $('.quote-form--email').addClass('error');
+//         $('.quote-form--email').parent().addClass('input-error');
+//         valid = false;
+//     }
+//     if (!emailValue.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+//         $('.form-item-email .desc').text('Email is not valid');
+//         $('.form-item-email .desc').show();
+//         $('.quote-form--email').addClass('error');
+//         $('.quote-form--email').parent().addClass('input-error');
+//         valid = false;
+//     }
+//     if (!phoneValue.trim()) {
+//         $('.form-item-phone .desc').show();
+//         $('.quote-form--phone').addClass('error');
+//         $('.quote-form--phone').parent().addClass('input-error');
+//         valid = false;
+//     }
+//     if (!zipValue.trim()) {
+//         $('.form-item-zip .desc').show();
+//         $('.quote-form--zip').addClass('error');
+//         $('.quote-form--zip').parent().addClass('input-error');
+//         valid = false;
+//     }
 
-    if(valid === ""){
-        valid = true;
-    }
+//     if(valid === ""){
+//         valid = true;
+//     }
     
 
+//     return valid;
+// }
+
+function validateInput(elem){
+    var elem = $(elem);
+    if(elem.length < 0 ){
+        return;
+    }
+    var elemParent = $(elem).parent();
+    var isEmail = elem.hasClass('quote-form--email');
+    var valid = true;
+ 
+    elem.removeClass('error');
+    elemParent.removeClass('input-error');
+    elemParent.find('.desc').hide();
+ 
+    if (!elem.val().trim() || (isEmail && !elem.val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) ) {
+        if(isEmail){
+            elemParent.find('.desc').text('Email is not valid');
+        }
+        elemParent.find('.desc').show();
+        elem.addClass('error');
+        elemParent.addClass('input-error');
+        valid = false;
+    }
+ 
     return valid;
 }
 
@@ -145,6 +170,7 @@ $(document).ready(function(){
     
     selectLoading('.form-item-year .chosen-container.chosen-container-single');
 });
+
 
 var apiUrl = "https://www.carqueryapi.com/api/0.3/";
 var selectYear = $('.form-item-year .quote-form--year');
@@ -227,7 +253,8 @@ $('body').on('change', '.form-item-make .quote-form--make', function(){
 
 });
 
-$('#quoteForm input').on('keypress', function(e){
+$('#quoteForm .form-item--input').on('keyup', function(e){
+    validateInput(this);
     var maxLength = parseInt($(this).attr('maxlength'));
     var curentValLength = $(this).val().length;
     curentValLength = curentValLength+1;
@@ -240,13 +267,44 @@ $('#quoteForm input').on('keypress', function(e){
     }
 });
 
+$('#quoteForm select').on('change', function(e){
+    validateInput(this);
+});
+
+
+$(".quote-form--phone").keydown(function (e) {
+    var numLength = $(this).val().length;
+    var numValue = $(this).val();
+    if (numLength == 3 && e.which != 8 && e.which != 0) {
+        $(this).val('(' + numValue + ')' + " ");
+    } else if ((numLength == 9 || numLength == 13) && e.which != 8 && e.which != 0) {
+        $(this).val(numValue + "-");
+    }
+});
+
 $(function(){
 
-    $('.numeric').on('keypress', function(e) {
+    $('.numeric').not('.quote-form--phone').on('keypress', function(e) {
       if(isNaN(this.value+""+String.fromCharCode(e.charCode))) return false;
     })
     .on("cut copy paste",function(e){
       e.preventDefault();
     });
   
+});
+
+$('.quote-form--phone').keydown(function(e)
+{
+    var key = e.charCode || e.keyCode || 0;
+    return (
+            key == 8 || 
+            key == 9 ||
+            key == 13 ||
+            key == 46 ||
+            key == 110 ||
+            key == 190 ||
+            (key >= 35 && key <= 40) ||
+            (key >= 48 && key <= 57) ||
+            (key >= 96 && key <= 105)
+           );
 });
